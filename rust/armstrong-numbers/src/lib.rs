36b2bdfa158fa30f76d2574bce569effb7643389
Brooklyn Photos <guoneng.list@gmail.com>
2019-08-30 00:28:33 +0200

    passed armstrong number tests

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+  let mut sum = 0_u32;
+  let length = count_digits(num);
+  for n in 0..length {
+    sum += digit_at(num, n).pow(length);
+  }
+  sum==num
+}
+
+fn count_digits(num: u32) -> u32 {
+    let b = num as f64;
+    (b.log10().floor() + 1_f64) as u32
+}
+
+fn digit_at(num: u32, index: u32) -> u32 {
+  num / 10_u32.pow(index) % 10
 }
